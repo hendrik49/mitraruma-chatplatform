aacfdf7ed2bfbbe044832f50774b612edb76e993
Hendrik <drikdoank@gmail.com>
2021-01-07 21:33:08 +0700

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,9 +52,6 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
                             @endif
                         @else
                             <li class="nav-item">
